e5f66c69a1d1ede83582d60b2e2f8711dfd66ab4
Nguyễn Hoàng Lâm <165152517+Kuren20052002@users.noreply.github.com>
2024-11-27 16:08:29 +0700

    Add json response for show and index

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -3,6 +3,11 @@ class KittensController < ApplicationController
 
   def index
     @kittens = Kitten.all
+
+    respond_to do |format|
+      format.html
+      format.json  { render json: @kittens }
+    end
   end
 
   def new
@@ -21,7 +26,12 @@ class KittensController < ApplicationController
     end
   end
 
-  def show; end
+  def show
+    respond_to do |format|
+      format.html
+      format.json  { render json: @kitten }
+    end
+  end
 
   def edit; end
 
